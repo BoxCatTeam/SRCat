85e059997e78e35762c3701fee6a60b3f5547f57
ohmyga <ohmyga@baka.show>
2023-05-22 22:19:04 +0800

    🛠️ debug mode does not initialize Sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 /// ===========================================================================
 /// Copyright (c) 2020-2023, BoxCat. All rights reserved.
 /// Date: 2023-05-06 19:23:46
-/// LastEditTime: 2023-05-19 11:36:49
+/// LastEditTime: 2023-05-22 19:06:02
 /// FilePath: /lib/main.dart
 /// ===========================================================================
 
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'package:srcat/application.dart';
@@ -112,14 +113,19 @@ Future<void> main() async {
   router.configureRoutes();
   Application.router = router.goRouter(Application.rootNavigatorKey);
 
-  /* Init Sentry */
-  await SentryFlutter.init(
-    (options) {
-      options.dsn = 'https://28b63ea28b5f4d1189ce8f7e1696d80a@o4505208485183488.ingest.sentry.io/4505208511791104';
-      options.tracesSampleRate = 1.0;
-    },
-    appRunner: () => runApp(const ProviderScope(child: SRCatAPP()))
-  );
+  // 如果是在开发模式下则不初始化 Sentry
+  if (kDebugMode) {
+    runApp(const ProviderScope(child: SRCatAPP()));
+  } else {
+    /* Init Sentry */
+    await SentryFlutter.init(
+      (options) {
+        options.dsn = 'https://28b63ea28b5f4d1189ce8f7e1696d80a@o4505208485183488.ingest.sentry.io/4505208511791104';
+        options.tracesSampleRate = 1.0;
+      },
+      appRunner: () => runApp(const ProviderScope(child: SRCatAPP()))
+    );
+  }
 }
 
 class SRCatAPP extends ConsumerStatefulWidget {
