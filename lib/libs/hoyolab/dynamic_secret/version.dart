5ba70d72dce7ba2735af94a3b9ff3cd29e880519
ohmyga <ohmyga@baka.show>
2023-10-27 15:36:07 +0800

    🔥 remove old files

--- lib/libs/hoyolab/dynamic_secret/version.dart
+++
@@ -1,8 +0,0 @@
-/// ===========================================================================
-/// Copyright (c) 2020-2023, BoxCat. All rights reserved.
-/// Date: 2023-05-27 12:23:39
-/// LastEditTime: 2023-05-27 12:23:48
-/// FilePath: /lib/libs/hoyolab/dynamic_secret/version.dart
-/// ===========================================================================
-
-enum HoYoLabDynamicSecretVersion { v1, v2 }
